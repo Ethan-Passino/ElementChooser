cf51591b0d95a489d2352aa8eb34a74c41c1ce51
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-09-27 16:44:54 -0400

    Add files via upload

---
+++ src/main/java/me/TurtlesAreHot/ElementChooser/commands/Choose.java
@@ -0,0 +1,82 @@
+package me.TurtlesAreHot.ElementChooser.commands;
+
+import com.projectkorra.projectkorra.BendingPlayer;
+import com.projectkorra.projectkorra.Element;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Choose implements CommandExecutor {
+
+    public String[] getValidElements(Player p) {
+        String[] elements = new String[9];
+        if(p.hasPermission("elementchooser.choose.water")) {
+            elements[0] = ChatColor.AQUA + "water";
+        }
+        if(p.hasPermission("elementchooser.choose.air")) {
+            elements[1] = ChatColor.GRAY + "air";
+        }
+        if(p.hasPermission("elementchooser.choose.fire")) {
+            elements[2] = ChatColor.RED + "fire";
+        }
+        if(p.hasPermission("elementchooser.choose.earth")) {
+            elements[3] = ChatColor.GREEN + "earth";
+        }
+        if(p.hasPermission("elementchooser.choose.chi")) {
+            elements[4] = ChatColor.GOLD + "chi";
+        }
+        if(p.hasPermission("elementchooser.choose.avatar")) {
+            elements[5] = ChatColor.DARK_PURPLE + "avatar";
+        }
+        if(p.hasPermission("elementchooser.choose.spirit")) {
+            elements[6] = ChatColor.DARK_AQUA + "spirit";
+        }
+        if(p.hasPermission("elementchooser.choose.darkspirit")) {
+            elements[7] = ChatColor.BLUE + "darkspirit";
+        }
+        if(p.hasPermission("elementchooser.choose.lightspirit")) {
+            elements[8] = ChatColor.YELLOW + "lightspirit";
+        }
+        return elements;
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(!(sender instanceof Player)) {
+            sender.getServer().getLogger().info("You cannot use this plugin in console!");
+            return false;
+        }
+        Player p = (Player) sender;
+        BendingPlayer bp = BendingPlayer.getBendingPlayer(p);
+        if(label.equalsIgnoreCase("choose")) {
+            //Permissions are: elementchooser.choose.element
+            if(args.length != 1) {
+                String[] elements = getValidElements(p);
+                String elementString = "";
+                for(int i = 0; i < elements.length; i++) {
+                    if(elements[i] == null) {
+                        continue;
+                    }
+                    if(i == 8) {
+                        elementString += elements[i];
+                        break;
+                    }
+                    elementString += elements[i] + ChatColor.WHITE + ", ";
+
+                }
+                p.sendMessage("Valid Elements: " + elementString);
+                return false;
+            }
+        }
+        else {
+            String element = args[0].toLowerCase();
+            if(!(p.hasPermission("elementchooser.choose." + element))) {
+                p.sendMessage(ChatColor.RED + " You either do not have permission to use this element or this element doesn't exist.");
+                return false;
+            }
+
+        }
+        return false;
+    }
+}
