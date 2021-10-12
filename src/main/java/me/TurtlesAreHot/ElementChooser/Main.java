befbe46afc8a899a803871c1a7b9551680a418cd
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-10-12 10:40:18 -0400

    Add choose command

--- src/main/java/me/TurtlesAreHot/ElementChooser/Main.java
+++ src/main/java/me/TurtlesAreHot/ElementChooser/Main.java
@@ -1,6 +1,7 @@
 package me.TurtlesAreHot.ElementChooser;
 
 import me.TurtlesAreHot.ElementChooser.commands.BlueFire;
+import me.TurtlesAreHot.ElementChooser.commands.Choose;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -8,6 +9,7 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         getCommand("bluefire").setExecutor(new BlueFire());
+        getCommand("choose").setExecutor(new Choose());
     }
 
     @Override
