65af66d6dba23baf14d5a80dc26d348e9f0dd3f0
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-12-06 16:14:40 -0500

    Add files via upload

--- src/main/java/me/TurtlesAreHot/ElementChooser/events/onElementAdd.java
+++ src/main/java/me/TurtlesAreHot/ElementChooser/events/onElementAdd.java
@@ -3,20 +3,34 @@ package me.TurtlesAreHot.ElementChooser.events;
 import com.projectkorra.projectkorra.BendingPlayer;
 import com.projectkorra.projectkorra.Element;
 import com.projectkorra.projectkorra.event.PlayerChangeElementEvent;
-import me.TurtlesAreHot.ElementChooser.commands.DarkSpirit;
-import me.xnuminousx.spirits.elements.SpiritElement;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import sun.security.provider.ConfigFile;
 
 public class onElementAdd implements Listener {
 
     @EventHandler
     public void onElementAdd(PlayerChangeElementEvent e) {
         if(e.getResult() == PlayerChangeElementEvent.Result.ADD) {
-
+            BendingPlayer bp = BendingPlayer.getBendingPlayer(e.getTarget());
+            if(e.getElement() == Element.AIR) {
+                bp.addSubElement(Element.SPIRITUAL);
+                bp.addSubElement(Element.FLIGHT);
+            }
+            if(e.getElement() == Element.FIRE) {
+                bp.addSubElement(Element.LIGHTNING);
+                bp.addSubElement(Element.COMBUSTION);
+            }
+            if(e.getElement() == Element.WATER) {
+                bp.addSubElement(Element.HEALING);
+                bp.addSubElement(Element.BLOOD);
+                bp.addSubElement(Element.ICE);
+                bp.addSubElement(Element.PLANT);
+            }
+            if(e.getElement() == Element.EARTH) {
+                bp.addSubElement(Element.METAL);
+                bp.addSubElement(Element.LAVA);
+                bp.addSubElement(Element.SAND);
+            }
         }
     }
 }
